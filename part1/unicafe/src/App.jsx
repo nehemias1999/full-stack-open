fdbd290897d67e4155b46f3963de004aeb6ba711
nehemias salazar <nehemiassala@gmail.com>
2023-10-29 23:17:44 -0300

    Finish 1.11*: unicafe step6

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -19,9 +19,14 @@ const Button = ({handleClick, text}) => {
 
 const StatisticLine = ({text, value}) => {
   return (
-    <p>
-      {text} {value}
-    </p>
+    <tr>
+      <td>
+        {text}
+      </td>
+      <td>
+        {value}
+      </td>
+    </tr>
   )
 }
 
@@ -34,14 +39,16 @@ const Statistics = ({good, neutral, bad}) => {
     const positive = (good / total) * 100;
 
     return (
-      <>
-        <StatisticLine text='good' value={good}/>
-        <StatisticLine text='neutral' value={neutral}/>
-        <StatisticLine text='bad' value={bad}/>
-        <StatisticLine text='all' value={total}/>
-        <StatisticLine text='average' value={average}/>
-        <StatisticLine text='positive' value={positive + ' %'}/>
-      </>
+      <table>
+        <tbody>
+          <StatisticLine text='good' value={good}/>
+          <StatisticLine text='neutral' value={neutral}/>
+          <StatisticLine text='bad' value={bad}/>
+          <StatisticLine text='all' value={total}/>
+          <StatisticLine text='average' value={average}/>
+          <StatisticLine text='positive' value={positive + ' %'}/>
+        </tbody>
+      </table>
     )
   } 
     
