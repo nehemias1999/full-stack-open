c88f8df433a43e67cffc730725bfc12c28c5c1f0
nehemias salazar <nehemiassala@gmail.com>
2023-10-31 22:34:06 -0300

    Finish 2.11: The Phonebook Step6

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,21 +1,25 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 import Title from './components/Title'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newSearch, setNewSearch] = useState('')
   const [newSearchList, setNewSearchList] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons')
+         .then(response => {
+            setPersons(response.data)
+         })
+  }, 
+  [])
 
   const handleSearchChange = (event) => {
     setNewSearch(event.target.value)
