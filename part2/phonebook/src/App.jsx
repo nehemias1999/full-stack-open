c8a675ecb9161e941bae9cf864ec3ea3cba9d8bb
nehemias salazar <nehemiassala@gmail.com>
2023-11-01 23:01:19 -0300

    Finish 2.12: The Phonebook step7

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -46,11 +46,15 @@ const App = () => {
     
     const personObject = {
       name: newName,
-      number: newNumber,
-      id: persons.length + 1
+      number: newNumber
     }
+
+    axios.post('http://localhost:3001/persons', personObject)
+         .then(response => {
+          setPersons(persons.concat(response.data))
+         }
+    )
   
-    setPersons(persons.concat(personObject))
     setNewName('')
     setNewNumber('')
 
